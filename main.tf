ed7485ab0313d3d6f0157a6b61fbea9d4a16aba6
hemanth <hemanthtadikonda29@gmail.com>
2025-01-18 16:52:10 +0530

    User-Info-app  Iac_code

---
+++ main.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "main" {
+  name        = var.internal ? "${var.env}-int-lb-sg" : "${var.env}-pub-lb-sg"
+  description = var.internal ? "${var.env}-int-lb-sg" : "${var.env}-pub-lb-sg"
+  vpc_id      = var.internal ?  var.vpc_id : var.default_vpc_id
+  tags        = merge(local.tags , { Name = var.internal ? "${var.env}-int-lb-sg" : "${var.env}-pub-lb-sg" })
+
+  ingress {
+    description = "APP"
+    from_port   = var.sg_port
+    to_port     = var.sg_port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_ingress_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+  }
+
+}
+
+resource "aws_lb" "main" {
+  name               = var.internal ? "${var.env}-int-lb-sg" : "${var.env}-pub-lb-sg"
+  internal           = var.internal
+  load_balancer_type = var.load_balancer_type
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.subnets
+  tags               = merge(local.tags , { Name = var.internal ? "${var.env}-int-lb" : "${var.env}-pub-lb" })
+
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Can you try with HostHeader"
+      status_code  = "200"
+    }
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "internal" {}
+variable "env" {}
+variable "tags" {}
+variable "vpc_id" {}
+variable "default_vpc_id" {}
+variable "sg_port" {}
+variable "sg_ingress_cidr" {}
+variable "load_balancer_type" {}
+variable "subnets" {} #list
